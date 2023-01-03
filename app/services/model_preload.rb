b188d3c282efb8dfe0e9cdb544bd3e33e7a1fd34
jiyong-jung-ringleplus <yjiong.jung@ringleplus.com>
2023-01-03 19:11:53 +0900

    fix: model preload

--- app/services/model_preload.rb
+++ app/services/model_preload.rb
@@ -5,11 +5,12 @@ class ModelPreload < ApplicationService
     end
 
     def call(indicator)
+        @indicator = indicator
         preload_model
     end
 
     def preload_model
-        indicator_array = indicator.map {|k, v| {"#{k}": v}}
+        indicator_array = @indicator.map {|k, v| {"#{k}": v}}
         @result = @model.select{ |entity|
             select_indicator = indicator_array.inject(true) {|cum, entry| 
                 cum &&
