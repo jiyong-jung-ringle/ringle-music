0e45ab06e9e512026aa19b0194e5a9714e00ffa1
jiyong-jung-ringleplus <yjiong.jung@ringleplus.com>
2023-01-09 14:32:50 +0900

    fix: prevent sql injection

--- app/services/virtual_column_service/get_similarity_score.rb
+++ app/services/virtual_column_service/get_similarity_score.rb
@@ -1,37 +1,37 @@
 module VirtualColumnService
-    class GetSimilarityScore < ApplicationService
+  class GetSimilarityScore < ApplicationService
+    def initialize(model, string, attribute_names)
+      @model = model
+      @sanitized_string = ActiveRecord::Base.connection.quote(string) # Prevent SQL injection
+      @attribute_names = attribute_names
+    end
 
-        def initialize(model, string, attribute_names)
-            @model = model
-            @string = string
-            @attribute_names = attribute_names
-        end
+    def call
+      get_score_indicators
+      get_select_indicator
+      get_scoring_model
+    end
 
-        def call
-            get_score_indicators
-            get_select_indicator
-            get_scoring_model 
-        end
-
-        private
-
+      private
         def get_score_indicators
-            @score_indicators = @attribute_names.map {|attribute_name| score_indicator(attribute_name)}.join("+")
+          @score_indicators = @attribute_names.map { |attribute_name| score_indicator(attribute_name) }.join("+").squish
         end
 
         def score_indicator(attribute_name)
-            "(LOWER(#{attribute_name}) SOUNDS LIKE LOWER('#{@string}'))/4 +
-            ((LOWER(#{attribute_name}) LIKE LOWER('%#{@string}%'))+(LOWER('#{@string}') LIKE LOWER(CONCAT('%', #{attribute_name}, '%')))+(LOWER(#{attribute_name}) LIKE LOWER('#{@string}')))*3/4"
+          """(LOWER(#{attribute_name}) SOUNDS LIKE LOWER(#{@sanitized_string}))/4 +
+            (
+                (LOWER(#{attribute_name}) LIKE LOWER(CONCAT('%', #{@sanitized_string}, '%'))) +
+                (LOWER(#{@sanitized_string}) LIKE LOWER(CONCAT('%', #{attribute_name}, '%'))) +
+                (LOWER(#{attribute_name}) LIKE LOWER(#{@sanitized_string}))
+            )*3/4"""
         end
 
         def get_select_indicator
-            @select_indicator = "#{@score_indicators} as score"
+          @select_indicator = "#{@score_indicators} as score"
         end
 
         def get_scoring_model
-            @model.select(@select_indicator)
+          @model.select(@select_indicator)
         end
-
-    end
-
+  end
 end
